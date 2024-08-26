0fb77902ff4c8040965237e6f5031621a498bc1d
SB <re323132020@outlook.com>
2024-08-26 10:32:10 +0800

    Create database.js

---
+++ xtcres/database.js
@@ -0,0 +1,21 @@
+# Nothing yet
+export const categories = [
+  # dir, zh name, en name 
+  ["magisk", "Magisk模块", "Magisk modules"],
+  ["xposed", "Xposed模块", "Xposed modules"],
+  ["apps", "手表应用", "Applications"]
+]
+export const data = {
+  "magisk": {
+    "sub": false,
+    "data": []
+  },
+  "xposed": {
+    "sub": false,
+    "data": []
+  },
+  "apps": {
+    "sub": true,
+    "data": []
+  }
+}
